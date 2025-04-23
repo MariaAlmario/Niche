2b5090a06a3a74ad7efe098c15fd216720a85b32
Juan-Caicedo27 <juan.caicedoc@uniagustiniana.edu.co>
2025-04-23 15:34:14 -0500

    Update reloj.js

--- reloj.js
+++ reloj.js
@@ -1,33 +1,5 @@
-// Función que actualiza la hora  
- function updateTime() {
-    
-    const timeElement = document.getElementById('time');
-
-    // nuevo objeto de fecha con   hora actual
-    const now = new Date();
-
-    // Obtiene las horas, minutos y segundos del objeto Date
-    const hours = String(now.getHours()).padStart(2, '0');   // Asegura que las horas tengan dos dígitos
-    const minutes = String(now.getMinutes()).padStart(2, '0'); //   minutos tengan dos dígitos
-    const seconds = String(now.getSeconds()).padStart(2, '0'); //  segundos tengan dos dígitos
-
-    // Actualiza el contenido del elemento 'time' 
-    timeElement.textContent = `${hours}:${minutes}:${seconds}`;
-}
-
-//   cada segundo (1000 milisegundos)
-setInterval(updateTime, 1000);
-
-// mostrar la hora al cargar la página
-updateTime();
-
-//slider//
-
 let slideIndex = 1;
-
-document.addEventListener("DOMContentLoaded", function () {
-    showSlides(slideIndex);
-});
+showSlides(slideIndex);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -40,7 +12,7 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     let slides = document.querySelectorAll(".slider");
-    let quadrates = document.querySelectorAll(".quadrate");
+    let quadrates = document.querySelectorAll(".quadrate"); 
 
     if (n > slides.length) {
         slideIndex = 1;
@@ -50,13 +22,13 @@ function showSlides(n) {
     }
 
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = "none"; 
     }
 
-    for (i = 0; i < quadrates.length; i++) {
-        quadrates[i].className = quadrates[i].className.replace(" active", "");
+    for (i = 0; i < quadrates.length; i++) { 
+        quadrates[i].className = quadrates[i].className.replace(" active", ""); 
     }
 
-    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block"; 
     quadrates[slideIndex - 1].className += " active";
 }
